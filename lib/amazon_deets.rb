2b3ef6612fbdcc225c0f6726cb9cda00bf47dc9f
Brian Lauber <constructible.truth@gmail.com>
2013-08-18 01:39:21 -0400

    Added scrapers for title, list price, current price, rating, and reviews

--- lib/amazon_deets.rb
+++ lib/amazon_deets.rb
@@ -1,14 +1,64 @@
 require 'logbert'
 require 'mechanize'
 
-module Amazon
+module AmazonDeets
 
-  class Deets
+  class Grabber
+    RatingRegex  = /(.+)\s+out\sof/
+    ReviewsRegex = /(\d+)/
 
     attr_accessor :agent
 
-    def initialize(agent: nil)
+    def initialize(agent: Mechanize.new)
       @agent = agent
+    end
+
+    def title
+      agent.page.search("//span[@id='btAsinTitle']").text
+    end
+
+
+    def list_price
+      nodes = agent.page.search("//span[@id='listPriceValue']")
+      if nodes.any?
+        return nodes.text
+      else
+        return current_price
+      end
+    end
+
+    def current_price
+      agent.page.search("//span[@id='actualPriceValue']").text
+    end
+
+
+    def rating
+      text = agent.page.search("//div[@id='acr']//span[@title]").text
+      m = RatingRegex.match(text)
+      m[1].to_f
+    end
+
+    def reviews
+      text = agent.page.search("//div[@id='acr']//div[contains(@class, 'acrCount')]").text
+      m = ReviewsRegex.match(text)
+      m[1].to_i
+    end
+
+
+    def details_hash
+      return {
+        title:         title,
+        list_price:    list_price,
+        current_price: current_price,
+        rating:        rating,
+        reviews:       reviews
+      }
+    end
+
+
+    def grab(url)
+      agent.get(url)
+      details_hash
     end
 
   end
